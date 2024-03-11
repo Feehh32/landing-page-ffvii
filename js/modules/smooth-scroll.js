23d5d521c3419f5a047765f1095f07011bff84d2
Fernando Pereira <fernando.mkv@gmail.com>
2024-03-11 15:06:58 -0300

    finalizando a animacao ao scroll

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -2,6 +2,9 @@ export default class SmoothScroll {
   constructor(links, options) {
     this.linksList = [...document.querySelectorAll(links)];
 
+    // define se as opçoes de scroll serão as predefinidas ou
+    //  se serão predefinidas pelo usuário da classe
+
     if (options === undefined) {
       this.options = { behavior: "smooth", block: "start" };
     } else {
@@ -11,6 +14,9 @@ export default class SmoothScroll {
     this.scrollToSection = this.scrollToSection.bind(this);
   }
 
+  // captura o href da sessão correspondente ao link e
+  // adiciona o efeito de scroll suave de acordo com o options
+
   scrollToSection(e) {
     e.preventDefault();
     const href = e.currentTarget.getAttribute("href");
@@ -18,6 +24,7 @@ export default class SmoothScroll {
     section.scrollIntoView(this.options);
   }
 
+  // adiciona um evento de click e a função de scroll suave em cada link interno
   addEventLink() {
     this.linksList.forEach((link) => {
       link.addEventListener("click", this.scrollToSection);
@@ -26,6 +33,7 @@ export default class SmoothScroll {
 
   init() {
     if (this.linksList.length) {
+      // retira o link que deve levar ao trailer
       this.linksList.pop();
       this.addEventLink();
     }
